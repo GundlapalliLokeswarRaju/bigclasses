c7cafa8d6b91b9ba11c7e431c5431e59e62c6a91
GundlapalliLokeswarRaju <lokeshgundlapalli143@gmail.com>
2025-05-26 22:06:45 +0530

    file changed

--- bigclasses.ai/src/lib/axiosConfig.ts
+++ bigclasses.ai/src/lib/axiosConfig.ts
@@ -3,7 +3,7 @@ import { toast } from '@/hooks/use-toast';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://13.201.139.142:8000/api',
-  baseURL: 'https://www.bigclasses.ai/api',
+  baseURL: 'http://13.201.139.142:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
